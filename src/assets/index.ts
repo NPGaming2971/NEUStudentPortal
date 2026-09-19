00253e80f7785c058d6521301cefed44faadbf92
NPGaming2971 <haht245@gmail.com>
2026-09-19 13:02:03 +0700

    remove unrelated assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,21 +1,14 @@
 // Logo
 import imageLogo from "./image_logo.png";
 
-// Banner
-import banner1 from "./banner_1.png";
-import banner2 from "./banner_2.png";
-
 // Background
 import loginBackground from "./login_background.png";
 
 const assets = {
 	imageLogo,
-
-	banner1,
-	banner2,
 	loginBackground,
 };
 
 export default assets;
 
-export { imageLogo, banner1, banner2, loginBackground };
+export { imageLogo, loginBackground };
